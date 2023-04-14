606d5b84b100a5a2a227f5df189fb99415cfdca9
ShaindalDev <89133440+ShaindalDev@users.noreply.github.com>
2023-04-14 20:16:08 +0200

    initial commit, added some structure in folders

---
+++ src/components/register.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+
+
+
+
+// {
+//     "name": "Robin Johnsen",
+//     "email": "Shaindal@stud.noroff.no",
+//     "avatar": "string",
+//     "venueManager": true,
+//     "password": "shaindal1234"
+//   }
