639e7897cf62c46e7ef537e565d016ac2278505c
ShaindalDev <89133440+ShaindalDev@users.noreply.github.com>
2023-05-22 11:48:30 +0200

    fiddling with searchbar and booking section. -searchbar and booking not working

--- src/components/BookingCalender.jsx
+++ src/components/BookingCalender.jsx
@@ -1,25 +1,25 @@
-import React from "react";
-import { getDatesBetween } from "../js/getDatesBetween";
-import { useVenues } from "../hooks/useVenues"
-import { useQuery } from "@tanstack/react-query";
+// import React from "react";
+// import { getDatesBetween } from "../js/getDatesBetween";
+// import { useVenues } from "../hooks/useVenues"
+// import { useQuery } from "@tanstack/react-query";
 
-const BookingCalender = () => {
-  const [data] = useQuery();
-  const {}
-  return (
-    <div>
-      <DatePicker
-        selected={startDate}
-        onChange={onChange}
-        startDate={startDate}
-        endDate={endDate}
-        excludeDates={[bookedDates]}
-        selectsRange
-        selectsDisabledDaysInRange
-        inline
-    />
-    </div>
-  )
-}
+// const BookingCalender = () => {
+//   const [data] = useQuery();
+//   const {}
+//   return (
+//     <div>
+//       <DatePicker
+//         selected={startDate}
+//         onChange={onChange}
+//         startDate={startDate}
+//         endDate={endDate}
+//         excludeDates={[bookedDates]}
+//         selectsRange
+//         selectsDisabledDaysInRange
+//         inline
+//     />
+//     </div>
+//   )
+// }
 
-export default BookingCalender
+// export default BookingCalender
